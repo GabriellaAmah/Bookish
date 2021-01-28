caf01093635cd9dff1c99c3265e66effb4f95195
gabbie amah <gabbieamah49@gmail.com>
2021-01-29 00:56:55 +0100

    removed the build folder

--- main/src/routers/client.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const clientRouter = express.Router();
-clientRouter.get('/', (_req, res) => {
-    return res.status(200).json({
-        message: 'hello welcome to Ebook ts'
-    });
-});
-export default clientRouter;
--- main/src/server/app.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as bodyParser from 'body-parser';
-import cors from 'cors';
-import clientRouter from '../routers/client';
-const App = express();
-App.use(cors());
-App.use(bodyParser.json());
-App.use(clientRouter);
-export default App;
